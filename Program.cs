a261b587c4ff3a9bf77c608927918787840b279e
KessyRamos <90793682+KessyRamos@users.noreply.github.com>
2021-09-29 09:58:14 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ListaForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ListaArray = new string[5];
+
+            ListaArray[0] = "Arroz";
+            ListaArray[1] = "Feijão";
+            ListaArray[2] = "Leite";
+            ListaArray[3] = "Açucar";
+            ListaArray[4] = "Macarrão";
+
+            Console.WriteLine("");
+
+            foreach(string Lista in ListaArray)
+            {
+                Console.WriteLine(Lista);
+            }
+            Console.WriteLine("");
+        }
+    }
+}
